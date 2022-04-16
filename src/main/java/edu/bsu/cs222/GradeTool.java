175205613eff202b331c1430095d93c6bbe59dce
Kameron Rogers <karogers3@bsu.edu>
2022-04-15 22:46:54 -0400

    Refactor variable names in GradeTool to better adhere to CC2
    Replaced variable names with intention-revealing names. Also, removed
    unnecessary prefaces (edu.bsu.cs222) to make the code more readable.

--- src/main/java/edu/bsu/cs222/GradeTool.java
+++ src/main/java/edu/bsu/cs222/GradeTool.java
@@ -6,26 +6,26 @@ import java.util.List;
 /**
  * Converts all the values given as input into letter grades
  * following <a href="https://www.cs.bsu.edu/~pvgestwicki/misc/grading.shtml">triage grading</a>.
- *
+ * <p>
  * Plus and minus grades are omitted for simplicity.
  */
 public class GradeTool {
     public static void main(String[] args) {
-        edu.bsu.cs222.GradeFile parser = new edu.bsu.cs222.GradeFile();
-        List<Float> list = null;
+        GradeFile percentageParser = new GradeFile();
+        List<Float> percentages = null;
         try {
-            list = parser.parse(System.in);
+            percentages = percentageParser.parse(System.in);
         } catch (IOException ioException) {
             System.out.println("There was a problem reading from the input stream.");
-        } catch (edu.bsu.cs222.GradeFile.ParseException parseException) {
+        } catch (GradeFile.ParseException parseException) {
             System.out.println("Invalid input format.");
         }
 
-        assert list != null;
+        assert percentages != null;
 
-        Convert policy = new Convert();
-        for (float number : list) {
-            System.out.println(policy.toGrade(number));
+        Convert triage = new Convert();
+        for (float percent : percentages) {
+            System.out.println(triage.toGrade(percent));
         }
     }
 }
