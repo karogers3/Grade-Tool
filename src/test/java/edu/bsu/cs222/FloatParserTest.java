a963ccc2c4659809d19aa12996f89ff17a59fe47
Kameron Rogers <karogers3@bsu.edu>
2022-04-16 15:34:50 -0400

    Refactor FloatParserTest in accordance with CC2 and CC3
    Create method which converts resource files into input streams to avoid
    DRY code.
    
    Rename methods to more accurately reflect the production code they are
    testing.
    
    Consider adding parameterized test, but decided not to as the test
    would be testing two distinct scenarios.

--- src/test/java/edu/bsu/cs222/FloatParserTest.java
+++ src/test/java/edu/bsu/cs222/FloatParserTest.java
@@ -4,27 +4,31 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class FloatParserTest {
+    private final FloatParser parser = new FloatParser();
+
+    private InputStream convertResourceToInputStream(String fileName) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+    }
 
     @Test
-    public void testParse_emptyFile_emptyList() throws IOException {
-        FloatParser parser = new FloatParser();
-        List<Float> values = parser.parseForFloats(Thread.currentThread().getContextClassLoader().getResourceAsStream("empty.txt"));
+    public void testParseForFloats_emptyFile_emptyList() throws IOException {
+        List<Float> values = parser.parseForFloats(convertResourceToInputStream("empty.txt"));
         Assertions.assertEquals(0, values.size());
     }
 
     @Test
-    public void testParse_threeItems_sizeIsThree() throws IOException {
-        FloatParser parser = new FloatParser();
-        List<Float> values = parser.parseForFloats(Thread.currentThread().getContextClassLoader().getResourceAsStream("three.txt"));
+    public void testParseForFloats_threeItems_sizeIsThree() throws IOException {
+        List<Float> values = parser.parseForFloats(convertResourceToInputStream("three.txt"));
         Assertions.assertEquals(3, values.size());
     }
 
     @Test
-    public void testParse_invalidFile_throwsCheckedException() {
-        FloatParser parser = new FloatParser();
-        Assertions.assertThrows(FloatParser.ParseException.class, ()-> parser.parseForFloats(Thread.currentThread().getContextClassLoader().getResourceAsStream("invalid.txt")));
+    public void testParseForFloats_invalidFile_throwsCheckedException() {
+        Assertions.assertThrows(FloatParser.ParseException.class, () ->
+                parser.parseForFloats(convertResourceToInputStream("invalid.txt")));
     }
 }
